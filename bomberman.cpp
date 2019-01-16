3158ed0e9fed8e0edb50c268a03c264841892a16
Krishna Kumar Yadav <krikalyt@gmail.com>
2019-01-17 01:30:38 +0530

    final answer

--- bomberman.cpp
+++ bomberman.cpp
@@ -3,9 +3,10 @@
 #include <algorithm>
 #include <string>
 using namespace std;
-vector<string> bomberMan(int n, vector<string> grid) {
+vector<string> bomberManHelper(int n, vector<string> grid) {
     int r = grid.size();
     int c = grid[0].size();
+    vector<string> gridtemp;
     vector<pair<int,int>> temp;
     for(int i=0;i<=(3)&&i<=n;i++) {
         if (i == 0) {
@@ -24,7 +25,7 @@ vector<string> bomberMan(int n, vector<string> grid) {
                     grid[j].at(k) = 'O';
                 }
             }
-
+            gridtemp = grid;
         }
 
         if (i == 3) {
@@ -47,59 +48,51 @@ vector<string> bomberMan(int n, vector<string> grid) {
             temp.clear();
         }
     }
-
-
-    for(int i=4;i<=(n);i++)
-    {
-        int remainder = i%2;
-        if(remainder==0)
-        {
-            for (int j = 0; j < r; ++j) {
-                for (int k = 0; k < c; ++k) {
-                    if(grid[j][k]=='O')
-                    {
-                        temp.push_back(pair<int,int>(j,k));
-                    }
-                }
-            }
-
-            for (int j = 0; j < r; ++j) {
-                for (int k = 0; k < c; ++k) {
-                    grid[j].at(k) = 'O';
-                }
-            }
-            if(i==n)
-                return grid;
-            continue;
-        }
-
-        if(remainder==1)
-        {
-            for(auto item: temp)
-            {
-                grid[item.first].at(item.second) = '.';
-                if(item.first!=(r-1))
-                {
-                    grid[item.first+1].at(item.second) = '.';
-                }
-
-                if(item.first!=0)
-                {
-                    grid[item.first-1].at(item.second) = '.';
-                }
-                if(item.second!=(c-1))
-                {
-                    grid[item.first].at(item.second+1) = '.';
-                }
-                if(item.second!=0)
-                {
-                    grid[item.first].at(item.second-1) = '.';
-                }
-            }
-            temp.clear();
-        }
-    }
     return grid;
+}
+
+
+vector<string> bomberMan(int n, vector<string> grid) {
+    vector<string> gridtemp = bomberManHelper(2,grid);
+    vector<string> gridtemp3 = bomberManHelper(3,grid);
+    vector<string> gridtemp3rev = bomberManHelper(3,gridtemp3);
+    if(n==0) {
+        return grid;
+    }
+    if(n==1)
+        return grid;
+    if(n==2)
+        return gridtemp;
+    if(n==3)
+        return gridtemp3;
+
+    if((n%2)==0)
+    {
+        return gridtemp;
+    }
+    int temp = n%20;
+    if(temp==1)
+        return gridtemp3rev;
+    if(temp==3)
+        return gridtemp3;
+    if(temp==5)
+        return gridtemp3rev;
+    if(temp==7)
+        return gridtemp3;
+    if(temp==9)
+        return gridtemp3rev;
+    if(temp==11)
+        return gridtemp3;
+    if(temp==13)
+        return gridtemp3rev;
+    if(temp==15)
+        return gridtemp3;
+    if(temp==17)
+        return gridtemp3rev;
+    if(temp==19)
+        return gridtemp3;
+    
+    return (vector<string>)NULL;
 }
 int main(void)
 {
