303d35856874ec3c1b701ac2f66453574afe95eb
Krishna Kumar Yadav <krikalyt@gmail.com>
2019-05-02 13:05:59 +0530

    Create init.cpp

---
+++ init.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define filei freopen("input.txt","r",stdin)
+#define fileo freopen("output.txt","w",stdout)
+#define print(a) cout << a << endl
+#define FOR(a,b) for(long i=a;i<b;i++)
+#define pb(a) push_back(a)
+#define first key
+#define second value
