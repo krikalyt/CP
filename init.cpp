6faeb5aa003b5eb330b8e5867d8ddf39c40f37b7
Krishna Kumar Yadav <krikalyt@gmail.com>
2019-08-22 00:19:05 +0530

    Update init.cpp

--- init.cpp
+++ init.cpp
@@ -1,31 +1,28 @@
-#include <iostream>
-#include <vector>
-#include <set> 
-#include <queue>
-#include <utility>
-#include <string>
-#include <stack>
-#include <list>
-#include <algorithm>
-#include <climits>
-#include <cmath>
-#include <cstdio>
-#include <unordered_map>
-#include <map>
-#include <chrono>
-#include <thread>
-using namespace std;
+//        Krishna Kumar Yadav          //
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define fileInput freopen("input.txt","r",stdin)
+#define fileOuput freopen("output.txt","w",stdout)
 #define prints(x) cout << x << " "
 #define printl(x) cout << x << "\n"
 #define print(x)  cout << x
-#define vinsert push_back
-#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
-#define filei freopen("input.txt","r",stdin)
-#define fileo freopen("ouput.txt","w",stdout)
-#define forl(a,b,c) for(int a=b;a<=c;a++)
-typedef long long int ll;
-int main(void)
+#define FOR(n) for(int i=0;i<n;i++)
+#define inputArr(arr,n) for(int i=0;i<n;i++)cin >> arr[i]
+using namespace std;
+//        Krishna Kumar Yadav          //
+
+int main()
 {
-    fastio;
+    fastIO;
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        inputArr(arr,n);
+    }
     return 0;
 }
