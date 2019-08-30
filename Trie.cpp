641387edc26459597188219015da32948fbdde18
Krishna Kumar Yadav <krikalyt@gmail.com>
2019-08-30 19:24:52 +0530

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -1,35 +1,56 @@
-//Trie using unordered_map
 #include <iostream>
 #include <unordered_map>
 #include <string>
 using namespace std;
-struct TrieNode{
-    bool end = false;
-    unordered_map<char,TrieNode*> abc;
+class Trie{
+private:
+    struct TrieNode{
+        bool end = false;
+        unordered_map<char,TrieNode*> abc;
+    };
+
+    TrieNode* root = new TrieNode();
+    bool search(TrieNode* root,string key)
+    {
+        TrieNode* curr = root;
+        for(int i=0;i<key.size();i++)
+        {
+            if(curr->abc.find(key[i]) == curr->abc.end())
+                return false;
+            curr = curr->abc[key[i]];
+        }
+        return curr->end;
+    }
+    
+    void insert(TrieNode* root,string key){
+        TrieNode* curr = root;
+        for(int i=0;i<key.size();i++)
+        {
+            if(curr->abc.find(key[i]) == curr->abc.end())
+                curr->abc.insert({key[i],new TrieNode()});
+            curr  = curr->abc[key[i]];
+        }
+        curr->end = true;
+    }
+
+public:
+
+    //User Level Code
+    void Insert_Trie(string abc)
+    {
+        insert(root,abc);
+    }
+
+    bool Search_Trie(string abc){
+        return search(root,abc);
+    }
+
+    //End
+    
+    ~Trie(){
+        delete root;
+    }
 };
-
-bool search(TrieNode* root,string key)
-{
-    TrieNode* curr = root;
-    for(int i=0;i<key.size();i++)
-    {
-        if(curr->abc.find(key[i]) == curr->abc.end())
-            return false;
-        curr = curr->abc[key[i]];
-    }
-    return curr->end;
-}
-
-void insert(TrieNode* root,string key){
-    TrieNode* curr = root;
-    for(int i=0;i<key.size();i++)
-    {
-        if(curr->abc.find(key[i]) == curr->abc.end())
-            curr->abc.insert({key[i],new TrieNode()});
-        curr  = curr->abc[key[i]];
-    }
-    curr->end = true;
-}
 
 int main()
 {
@@ -39,24 +60,23 @@ int main()
 	int test;
 	cin >> test; 
 	while(test--)
-    {
-        TrieNode* root = new TrieNode();
-        int N,Q;
-        cin >> N >> Q;
-        for(int i=0;i<N;i++)
-        {
-            string temp;
-            cin >> temp;   
-            insert(root,temp);
-        }
+    	{
+		Trie tr;
+		int N,Q;
+		cin >> N >> Q;
+		for(int i=0;i<N;i++)
+		{
+		    string temp;
+		    cin >> temp;   
+		    tr.Insert_Trie(temp);
+		}
 
-        for(int i=0;i<Q;i++)
-        {
-            string temp;
-            cin >> temp;
-            cout << search(root,temp) << '\n';
-        }
-        delete root;
-    }
+		for(int i=0;i<Q;i++)
+		{
+		    string temp;
+		    cin >> temp;
+		    cout << tr.Search_Trie(temp) << '\n';
+		}
+   	}
 	return 0;
 }
