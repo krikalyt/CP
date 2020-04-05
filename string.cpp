88cfcd8a9b968ef0522960fdeb759e4dd753447a
Krishna Kumar Yadav <krikalyt@gmail.com>
2020-04-04 23:47:07 -0700

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+int main()
+{
+    /*
+        string(str);
+        string(char*);
+        string  .resize(n);
+                .size();
+                .erase(pos,len);
+                .insert(pos,str);
+                .find(str) string::npos
+                .append(str), +
+                .push_back(char);
+                .pop_back();
+                .substr(pos,len);
+        str[i] -> char at i 
+        to_string(no|char);
+        stoi(str);
+        stoll(str);
+        stod(str);
+        stold(str);
+        
+        transform(sl.begin(), sl.end(), sl.begin(), ::tolower); 
+        transform(sl.begin(), sl.end(), sl.begin(), ::toupper); 
+        sort(s1.begin(),s1.end()); sorting according ascii of character
+    */
+    return 0;
+}
